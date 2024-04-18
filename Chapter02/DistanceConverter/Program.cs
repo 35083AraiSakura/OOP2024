30d15c7c271e1be9d72b9ff7a3939a19a93a19f3
AraiSakura <157446032+35083AraiSakura@users.noreply.github.com>
2024-04-18 14:48:25 +0900

    静的クラスに変更

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -5,26 +5,24 @@ namespace DistanceConverter {
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
                 //フィートからメートルへの対応表を出力
-                PrintFeetToMeterList(int.Parse(args[0]), 10);
+                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
             } else {
                 //メートルからフィートへの対応表を出力
-                PrintMeterToFeetList(1, 10);
+                PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));
             }
         }
 
 
         private static void PrintFeetToMeterList(int start, int stop) {
-            FeetConverter fc= new FeetConverter();
             for (int feet = start; feet <= stop; feet++) {
-                double meter = fc.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
 
         private static void PrintMeterToFeetList(int start, int stop) {
-            FeetConverter fc = new FeetConverter();
             for (int meter = start; meter <= stop; meter++) {
-                double feet = fc.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }        
