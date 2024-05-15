fbd5209a47105554d499b5bdf882fd9073e1aeeb
AraiSakura <157446032+35083AraiSakura@users.noreply.github.com>
2024-05-15 14:10:46 +0900

    教科書のプログラム実践

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,23 +7,24 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 1, 2, 5, 10, 4, 5 };
+            //var list = new List<string> {
+            var names = new List<string> {
+            "Tokyo",
+            "New Delhi",
+            "Bangkok",
+            "London",
+            "Paris",
+            "Berlin",
+            "Canberra",
+            "Hong Kong",
+            };
 
-            //int count = numbers.Count(n => n % 2 == 0);
-            //int count = Count(numbers, n => n.ToString().Contains('1'));
-            double num = numbers.Where(n => n > 5).Average();
-            int total = numbers.Where(n => n > 5).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(total);
+
+            IEnumerable<string> query = names.Where(s => s.Contains(' '))
+                                             .Select(s => s.ToUpper());
+
+            foreach (string s in query)
+                Console.WriteLine(s);
         }
-
-        //public static int Count(int[] numbers, Predicate<int> judge) {
-        //    int count = 0;
-        //    foreach (var n in numbers) {
-        //        if (judge(n) == true)
-        //            count++;
-        //    }
-        //    return count;
-        //}
     }
 }
