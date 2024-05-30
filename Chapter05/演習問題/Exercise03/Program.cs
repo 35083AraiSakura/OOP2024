e731bad30352e20d0f7e83dd9bc4dca7713b6fd0
AraiSakura <157446032+35083AraiSakura@users.noreply.github.com>
2024-05-30 15:59:47 +0900

    5章演習問題3_5,3_6 答え合わせ済み

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -9,6 +9,7 @@ namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
+            var text_a = "Jackdaws love,my big-sphinx-of_quartz";
 
             Exercise3_1(text);
             Console.WriteLine("-----");
@@ -23,6 +24,9 @@ namespace Exercise03 {
             Console.WriteLine("-----");
 
             Exercise3_5(text);
+            Console.WriteLine("-----");
+
+            Exercise3_6(text_a);
         }
 
         private static void Exercise3_1(string text) {
@@ -42,26 +46,34 @@ namespace Exercise03 {
 
         private static void Exercise3_3(string text) {
             int count = text.Split(' ').Length;
-            Console.WriteLine("単語数：{0}",count);
+            Console.WriteLine("単語数：{0}", count);
         }
 
         private static void Exercise3_4(string text) {
             var words = text.Split(' ').Where(s => s.Length <= 4);
-            foreach (var word in words)
-            {
+            foreach (var word in words) {
                 Console.WriteLine(word);
             }
         }
 
         private static void Exercise3_5(string text) {
             var words = text.Split(' ').ToArray();
-            
-            var sb= new StringBuilder();
+
+            var sb = new StringBuilder();
             foreach (var word in words) {
                 sb.Append(word);
                 sb.Append(' ');
             }
             Console.WriteLine(sb);
+        }
+
+        private static void Exercise3_6(string text) {
+            var array = text_a.Split(new[] { ' ', ',', '-', '_' }).ToArray();
+            foreach (var word in array)
+            {
+                Console.WriteLine(word);
+            }
+
         }
     }
 }
--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
-            //ar line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            //var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
 
         }
