18572eb325445f9fc9c3545f76a118389e2dcac7
AraiSakura <157446032+35083AraiSakura@users.noreply.github.com>
2024-06-05 10:23:22 +0900

    演習問題5.4答え合わせ済み

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
-            //var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
-
+            foreach (var item in line.Split(';')) {
+                var array = item.Split('=');
+                Console.WriteLine("{0}：{1}", ToJapanese(array[0]), array[1]);
+            }
         }
 
         //できたら以下のメソッドを完成させて利用する
-        //static string ToJapanese(string key) {
+        static string ToJapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家  ";
 
+                case "BestWork":
+                    return "代表作";
 
-        //}
+                case "Born":
+                    return "誕生年";
+
+            }
+            throw new ArgumentException("引数に誤りがあります");
+        }
     }
 }
 
