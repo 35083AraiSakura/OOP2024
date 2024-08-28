8e4b230c44fd4a9888d19e5bafcea7cf28891ae2
AraiSakura <157446032+35083AraiSakura@users.noreply.github.com>
2024-08-28 13:30:20 +0900

    演習問題12.1-3まで　答え合わせ後

---
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Exercise01 {
+    public class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
+
